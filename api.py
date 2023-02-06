57813ef9ba410147004cf23da2e8f9fb073ad8b8
Peter Tsz Fung Tam <petertam0712@hotmail.com>
2023-02-06 08:10:29 +0800

    fixed for some logic bug, and add one more function for process json file to database.

--- api.py
+++ api.py
@@ -3,27 +3,29 @@ from neo4j import GraphDatabase
 import csv
 
 # establish the connection
+"""
 with open("cred.txt") as fl:
     data = csv.reader(fl, delimiter=',')
     for row in data:
         username = row[0]  # account name:neo4j
         pwd = row[1]  # password:neo4j
         uri = row[2]
+"""
 
-print(username, pwd, uri)
-driver = GraphDatabase.driver(uri=uri, auth=(username, pwd))
+#print(username, pwd, uri)
+driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "12345678"))
 session = driver.session()
 api = Flask(__name__)
 
 
 @api.route("/create/<string:name>&<string:code>", methods=["GET", "POST"])
 def create_node(name, code):
-    q1 = """
+    query = """
     create (n:Stock{NAME:$name,CODE:$code})
     """
     map = {"name": name, "code": code}
     try:
-        session.run(q1, map)
+        session.run(query, map)
 
         return (f"stock code is created with stock name={name} and code={code}")
     except Exception as e:
@@ -31,27 +33,66 @@ def create_node(name, code):
 
 @api.route("/display", methods=["GET", "POST"])
 def display_node():
-    q2 = """
+    query = """
     match (n) return n.NAME as NAME, n.CODE as CODE
     """
-    results=session.run(q2)
+    results=session.run(query)
 
     data = results.data()
     return (jsonify(data))
 
-@api.route("/delete/<string:name>", methods=["GET", "POST"])
+@api.route("/update/<string:name>", methods=["PUT"])
+def update_node(name):
+    code = request.json.get('code')
+    session.run("MATCH (n:Stock {NAME: $name}) SET n.CODE = $code", name=name, code=code)
+    session.close()
+    return jsonify({'message': 'Stock info updated'})
+
+"""
+    guide = Guide.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    guide.title = title
+    guide.content = content
+
+    db.session.commit()
+    return guide_schema.jsonify(guide)
+"""
+
+@api.route("/delete/<string:name>", methods=["DELETE"])
 def delete_node(name):
-    q3 = """
+    query = """
     MATCH (n:Stock {NAME:$name})
     DELETE n
     """
 
     try:
-        session.run(q3)
+        session.run(query, name=name)
 
-        return (f"stock code {name}is deleted in the list")
+        return (f"stock code {name} is deleted in the list")
     except Exception as e:
         return (str('delete function can not be function'))
+
+import json
+@api.route("/processjson", methods=["POST"])
+def create_node_by_file():
+
+    query = """
+    create (n:Stock{NAME:$name,CODE:$code})
+    """
+
+    data = request.get_json()
+
+    for i in data:
+        code = i['CODE']
+        name = i['NAME']
+
+        map = {"name": name, "code": code}
+
+        session.run(query, map)
+
+    return display_node()
 
 
 if __name__ == "__main__":
